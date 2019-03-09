9dcce586a9d7c68b9250e0e42765bb27a39cddaa
Ilia Idakiev <ilia.idakiev@gmail.com>
2019-03-09 12:53:41 +0200

    fixes and minor version update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { QueryParamsStore } from 'query-params-store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'query-param-store';
   store: Observable<any>;
 
+  subscription: Subscription;
+
   constructor(private qpsStore: QueryParamsStore) {
     this.store = qpsStore.store;
-    this.store.subscribe((data) => console.log('app', data));
+    this.subscription = this.store.subscribe((data) => console.log('app', data));
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
