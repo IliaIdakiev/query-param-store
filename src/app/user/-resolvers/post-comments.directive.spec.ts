c722fdd92fe6b7c7a1d339c4cca27ff42fd1bc6f
Ilia Idakiev <ilia.idakiev@gmail.com>
2020-04-08 15:51:31 +0300

    removed post comment from users

--- src/app/user/-resolvers/post-comments.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PostCommentsDirective } from './post-comments.directive';
-
-describe('PostCommentsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PostCommentsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
