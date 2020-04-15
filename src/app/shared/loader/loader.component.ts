4a4b4b46f34742369fbad3872c40c33a336b0aff
Ilia Idakiev <ilia.idakiev@gmail.com>
2020-04-15 12:41:30 +0300

    app updates

--- src/app/shared/loader/loader.component.ts
+++ src/app/shared/loader/loader.component.ts
@@ -1,12 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.scss']
 })
-export class LoaderComponent {
+export class LoaderComponent implements AfterViewInit {
 
   @Input() isLoading = false;
 
+
+  showMessage = false;
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.showMessage = true;
+    }, 6000);
+  }
 }
